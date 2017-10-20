06e0421945b147ffe39188dfcabe78b42bafafb8
Emi Chen <emimbchen@gmail.com>
2017-10-20 13:32:33 -0500

    changed port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var userinputRouter = require('./routes/userinput_router.js');
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
